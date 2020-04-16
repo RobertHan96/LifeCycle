a35b94205125d5cb3e1b7bdfd00208a0d674ef69
RobertHan96 <yshan4329@gmail.com>
2020-04-16 21:17:01 +0900

    생명주기 기초 코드 추가 : onResume, onCreate

--- app/src/main/java/com/nexon/lifecycle/MainActivity.kt
+++ app/src/main/java/com/nexon/lifecycle/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nexon.lifecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -11,8 +12,13 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValus()
+        Log.d("MainActivity", "onCreate실행")
     }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume실행")
+    }
     override fun setupEvents() {
         nextBtn.setOnClickListener {
             val myIntent = Intent(mContext, NextActivity::class.java)
