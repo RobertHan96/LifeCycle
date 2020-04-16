c6b48e14b51474fa7445e838c5185bf9887cdd65
RobertHan96 <yshan4329@gmail.com>
2020-04-16 21:09:48 +0900

    다음 화면으로 이동하는 기능 추가

--- app/src/main/java/com/nexon/lifecycle/NextActivity.kt
+++ app/src/main/java/com/nexon/lifecycle/NextActivity.kt
@@ -1,27 +1,20 @@
 package com.nexon.lifecycle
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class NextActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_next)
         setupEvents()
         setValus()
     }
 
     override fun setupEvents() {
-        nextBtn.setOnClickListener {
-            val myIntent = Intent(mContext, NextActivity::class.java)
-            startActivity(myIntent)
-        }
     }
 
     override fun setValus() {
-
     }
 
 }
